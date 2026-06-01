f04691997d5f2e61fd4212b4211f2ae6622b94f8
Oak <oaksoeoo23@gmail.com>
2026-06-01 15:37:52 +0800

    fixed command bug

--- src/components/CommandPalette/CommandPalette.tsx
+++ src/components/CommandPalette/CommandPalette.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { Search, BookOpen, FileText, AlignLeft, ArrowRight, Command } from 'lucide-react';
+import { Search, BookOpen, FileText, AlignLeft, ArrowRight} from 'lucide-react';
 import { useGlobalSearch, type GlobalSearchResult } from '../../hooks/useGlobalSearch';
 import { useApp } from '../../context/AppContext';
 import { useChapterNavigation } from '../../hooks/useChapterNavigation';
